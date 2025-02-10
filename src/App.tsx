c72178ac8a7cde3eb73cdaab05d6a576a1d911af
mathbook3948 <wjddbsgh58@gmail.com>
2025-02-10 18:10:26 +0900

    vercel.json 추가

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<QR />} />
-            <Route path="/login" element={<Login />}/>
+          <Route path="/login" element={<Login />}/>
         </Routes>
       </BrowserRouter>
     </div>
